9efd51539c00033d5333b02afbaca765c1655e6f
Dave <davemh@users.noreply.github.com>
2025-09-23 18:58:11 -0700

    Update gif_to_png_zip.py
    Clean up formatting

--- gif_to_png_zip.py
+++ gif_to_png_zip.py
@@ -30,6 +30,5 @@ def gif_to_png_zip(gif_path, out_dir="frames", zip_name="frames.zip"):
     print(f"Created {zip_name} with {len(filenames)} frames.")
 
 # Allow script to be run directly, or module to be loaded without execution, from the same file.
-
 if __name__ == "__main__":
     gif_to_png_zip("input.gif")
